da2de2bda6e815bff20cc06fe53a296d04afe0ef
Trevor Kimenye <trevor@kimenye.com>
2012-01-20 17:57:23 +0300

    Added stylesheets and js

--- app.js
+++ app.js
@@ -34,5 +34,9 @@ app.configure('production', function(){
 
 app.get('/', routes.index);
 
-app.listen(3000);
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
+
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
