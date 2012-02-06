835b8e80e5962132fc913a3765764e42e1004294
Trevor Kimenye <trevor@kimenye.com>
2012-02-06 18:53:50 +0300

    Added support to select custom areas

--- public/javascripts/web.js
+++ public/javascripts/web.js
@@ -1,29 +1,48 @@
-function Selection(id) {
-	
-}
-
-
 $(document).ready(function() {
-	var geocoder;
-	var map;
-	var circle, rectangle;
-	var zonesLayer, pipeLayer;
-	var infowindow = new google.maps.InfoWindow();
+	var geocoder, map, circle, rectangle;
+	var zonesLayer, pipesLayer;
+
 	var PIPES_TABLE = 2852415;
 	var TEST_GEO = 2852795;
 	var ZONES_TABLE = 2852009;
-	
-	var selections;
-	
+	var drawingMode = true;
+	var selectedItem = null;
+		
+	/**
+	 * Initialize the drawing manager
+	 */
 	var drawingManager = new google.maps.drawing.DrawingManager({
 		drawingControlOptions: {
-			drawingModes: [google.maps.drawing.OverlayType.POLYGON, google.maps.drawing.OverlayType.RECTANGLE, google.maps.drawing.OverlayType.CIRCLE],
+			drawingModes: [google.maps.drawing.OverlayType.POLYGON, google.maps.drawing.OverlayType.RECTANGLE], //google.maps.drawing.OverlayType.CIRCLE],
 			position: google.maps.ControlPosition.TOP_RIGHT
 		}
 	});
 	
+	/**
+	 * Event listner for when a polygon is completed
+	 */
+	google.maps.event.addListener(drawingManager, 'polygoncomplete', function(polygon) {
+		searchCustom(polygon);
+	});
 	
-	function initialize() {
+	/**
+	 * Event listner for when a rectangle is completed
+	 */
+	google.maps.event.addListener(drawingManager, 'rectanglecomplete', function(rectangle) {
+		searchCustom(rectangle);
+	});
+	
+	function searchCustom(customArea) {
+		invalidate(selectedItem);
+		map.fitBounds(customArea.getBounds());
+		drawingMode = true;
+		$('#txt_where').val('Selected Area').attr('disabled','disabled');
+		$('#btn_clear').button( "option", "disabled", false );
+
+		selectedItem = customArea;
+	}
+	
+	function initMap() {
 		geocoder = new google.maps.Geocoder();
 		geocoder.geocode( { 'address':  'Mombasa, Coast'}, function(results, status) {
 			if (status == google.maps.GeocoderStatus.OK) {
@@ -43,9 +62,6 @@ $(document).ready(function() {
 				};
 				map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
 				drawingManager.setMap(map);
-				//loadJson(map);
-				//loadFusionTable(map);
-				//search($('#txt_where').val());
 			} 
 			else 
 			{
@@ -54,89 +70,44 @@ $(document).ready(function() {
 		});
 	}
 	
-	initialize();
-	
-	function loadFusionTable(map) {
-		zonesLayer = new google.maps.FusionTablesLayer(ZONES_TABLE, {
-			suppressInfoWindows: true
+	function initUI() {
+		$('#result_tabs').tabs();
+		$("#accordion").accordion({ header: "h4" });
+		$("#header").css({ opacity: 0.9 });
+		$("#sidebar").css({ opacity: 0.7 });
+		$("#btn_submit").button( { disabled: false });
+		$("#btn_clear").button( {disabled: true});
+		
+		$("#btn_clear").click(function(event) {
+			reset();
 		});
-		zonesLayer.setMap(map);
-	}
-	
-	function loadJson(map, center) {
-		console.log("Loading the json");
-		$.ajax({
-          type: "GET",
-          url: "javascripts/zones.json",
-          cache: false,
-          dataType: "json",
-          success: function(data) {
-				onJson(data, map);
+		
+		$('#btn_submit').click(function(event) {
+			var text = $('#txt_where').val();
+			if (text && selectedItem == null) {
+				search(text);
 			}
-        });
-	}
-	
-	function onJson(data,map) {
-		if (data && data.features) {
-			
-			var bd = new google.maps.LatLngBounds();
-			var ft = new GeoJSON(data, parcelStyle);
-			if (ft.length) {
-				for(var i=0;i<ft.length;i++) {
-					setupFeature(ft[i], map);
-					var p = ft[i].getBounds();
-					bd.union(p);
-				}
-			}
-			else
+			else if (selectedItem != null)
 			{
-				setupFeature(ft,map);
-				bd.union(ft.getBounds());
+				searchBounds(selectedItem.getBounds(), text);
 			}
-			map.fitBounds(bd);
-			map.setCenter(bd.getCenter());
-		}
-	}
-	
-	function setupFeature(feature,map) {
-		feature.setMap(map);
-		setInfoWindow(feature,map);
-	}
-	
-	function setInfoWindow (feature, map) {
-		google.maps.event.addListener(feature, "click", function(event) {
-			var content = "<div id='infoBox'><strong>Zone Properties</strong><br />";
-			for (var j in this.geojsonProperties) {
-				content += j + ": " + this.geojsonProperties[j] + "<br />";
+		});
+		
+		$("#txt_where").keypress(function(event) {
+			if ( event.which == 13 ) {
+				var text = $('#txt_where').val();
+				if (text)
+					search(text);
 			}
-			content += "</div>";
-			infowindow.setContent(content);
-			infowindow.position = event.latLng;
-			infowindow.open(map);
 		});
 	}
 	
-	var parcelStyle = {
-		strokeColor: "#FF7800",
-		strokeOpacity: 1,
-		strokeWeight: 1,
-		fillColor: "#46461F",
-		fillOpacity: 0.15
-	};
+	initMap();
+	initUI();
 	
-	function getZoomForBounds(bounds) {
-		var GLOBE_WIDTH = 256; // a constant in Google's map projection
-		var west = bounds.getSouthWest().lng();
-		var east = bounds.getNorthEast().lng();
-		var angle = east - west;
-		if (angle < 0) {
-		  angle += 360;
-		}
-		console.log($('#map_canvas').width());
-		var zoom = Math.round(Math.log($('#map_canvas').width() * 360 / angle / GLOBE_WIDTH) / Math.LN2);
-		return zoom;
-	}
-	
+	/** 
+	 * Extend Google Maps to get bounds from a polygon
+	 */	
 	if (!google.maps.Polygon.prototype.getBounds) {
         google.maps.Polygon.prototype.getBounds = function(latLng) {
             var bounds = new google.maps.LatLngBounds();
@@ -151,17 +122,7 @@ $(document).ready(function() {
             return bounds;
         }
     }
-	
-	$("#accordion").accordion({ header: "h4" });
-	$("#header").css({ opacity: 0.9 });
-	$("#sidebar").css({ opacity: 0.7 });
-	$("#btn_submit").button( { disabled: false });
-	$('#btn_submit').click(function(event) {
-		var text = $('#txt_where').val();
-		if (text) {
-			search(text);
-		}
-	});
+
 	
 	$(window).resize(function() {
 		if (map) {
@@ -169,37 +130,51 @@ $(document).ready(function() {
 		}
 	});
 	
-	$("#txt_where").keypress(function(event) {
-		if ( event.which == 13 ) {
-			search($('#txt_where').val());
-		}
-	});
-	
-	function loadPipes(map,bounds) {
-// /		debugger;
-		//where ST_INTERSECTS(geometry, RECTANGLE(LATLNG(49.289, -123.144), LATLNG(49.292, -123.138)))
-		
-		// var where = 'ST_INTERSECTS(geometry, RECTANGLE(LATLNG(' + bounds.getNorthEast().lat() + ',' + bounds.getNorthEast().lng() +'), LATLNG(' + bounds.getSouthWest().lat() +',' +  bounds.getSouthWest().lng() +')))';
-		// 		console.log(where);
-		// 		
-		// 		var c = 'ST_INTERSECTS(geometry, CIRCLE(LATLNG(' + bounds.getCenter().lat() + ',' + bounds.getCenter().lng() +'),1)';
-		// 		console.log(c);
-		
+	/**
+	 * Load the overlaps 
+	 */
+	function loadOverlays(bounds) {		
 		var rect = 'RECTANGLE(LATLNG(' + bounds.getSouthWest().lat() + ',' + bounds.getSouthWest().lng() +'), LATLNG(' + bounds.getNorthEast().lat() + ',' + bounds.getNorthEast().lng() +'))';
 		var where = 'ST_INTERSECTS(geometry,' + rect + ')';
 		
-		//circle = new google.maps.Circle({center: bounds.getCenter(), map: map, radius: 1000});
-		
-		var pipes = new google.maps.FusionTablesLayer({
+		pipesLayer = new google.maps.FusionTablesLayer({
 			query: {
 				select: 'geometry',
 				where: where,
 				from: PIPES_TABLE
 			}
 		});
-		pipes.setMap(map);
+		pipesLayer.setMap(map);
+		
+		zonesLayer = new google.maps.FusionTablesLayer({
+			query: {
+				select: 'geometry',
+				where: where,
+				from: ZONES_TABLE
+			}
+		});
+		zonesLayer.setMap(map);
 	}
 	
+	function searchBounds(bounds,text) {
+		deactivate();
+		loadOverlays(bounds);
+		
+		$('#sidebar').removeClass('hide');
+		var id = text.replace(/ /g,'');
+		$('#searches').append('<li id="li_result"><a href="#">' + text + '</a><a id="rm_result" href="#" class="ui-icon ui-icon-trash right"></li>');
+
+
+		$('#rm_result').click(function() {
+			// clearSearch(id);
+			reset();
+		});
+	}
+	
+	
+	/**
+	 * Search. Create a bounds using the geocode
+	 */
 	function search(text) {
 		geocoder.geocode( { 'address' : text + ', Mombasa'}, function(results,status) {
 			if (status == google.maps.GeocoderStatus.OK) {
@@ -207,51 +182,67 @@ $(document).ready(function() {
 				
 				console.log("Pos: " + point.location.lat() + "," + point.location.lng());
 				
-				if (rectangle) {
-					rectangle.setMap(null);
-					rectangle = null;
-				}
-				
+				invalidate(rectangle);	
 				rectangle = new google.maps.Rectangle({ bounds: point.viewport });
 				rectangle.setMap(map);
 				map.fitBounds(point.viewport);
-				
-				var id = text.replace(/ /g,'');
-				
-				$('#no-searches').addClass('hide');					
-				$('#searches').append('<li id="li_' + id + '"><a id="' + id +'" href="#">' + text + '</a><a id="rm_' + id +  '" href="#" class="ui-icon ui-icon-trash right"></li>');
-				$('#searches').resize();
-				$('#txt_where').attr('disabled','disabled');
-				$('#btn_submit').button( "option", "disabled", true );
-				loadPipes(map, point.viewport);
-
-				// $('#' + id).click(function() {
-				// 					if (rectangle) {
-				// 						rectangle.setMap(null);
-				// 						rectangle = null;
-				// 					}
-				// 					rectangle = new google.maps.Rectangle({ bounds: point.viewport });
-				// 					rectangle.setMap(map);
-				// 					map.fitBounds(point.viewport);
-				// 				});
-				
-				$('#rm_' + id).click(function() {
-					if (rectangle) {
-						rectangle.setMap(null);
-						rectangle = null;
-					}
-					
-					$('#li_' + id).remove();
-				});
-				
-				
+				searchBounds(point.viewport, text);
 			}
 			else
 			{
 				console.log("Geocode was not successful for the following reason: " + status);
 			}
-			// $('#loading').removeClass('overlay');
 		});
 	}
+	
+	
+	/**
+	 * Invalidate a map object
+	 */
+	function invalidate(obj,dontNullify) {
+		if (obj) {
+			obj.setMap(null);
+			if (dontNullify)
+				obj = null;
+		}
+	}
+	
+	/**
+	 * Deactivate when we are performing a search
+	 */
+	function deactivate() {
+		invalidate(drawingManager, true);
+		
+		$('#no-searches').addClass('hide');					
+		$('#searches').resize();
+		$('#txt_where').attr('disabled','disabled');
+		$('#btn_submit').button( "option", "disabled", true );
+		$('#btn_clear').button( "option", "disabled", false );
+	}
+	
+	/**
+	 * Reset the search
+	 */
+	function reset() {
+		invalidate(rectangle);
+		invalidate(zonesLayer);
+		invalidate(pipesLayer);
+		invalidate(selectedItem);
+		drawingManager.setMap(map);
+		
+		$('#txt_where').removeAttr('disabled').val('');
+		$('#btn_submit').button('option', "disabled", false);
+		$('#btn_clear').button( "option", "disabled", true );
+		$('#sidebar').addClass('hide');
+		$('#li_result').remove();
+	}
+	
+	/**
+	 * Clear the search and results
+	 */
+	// function clearSearch() {
+	// 		reset();
+	// 		
+	// 	}
 	
 });
