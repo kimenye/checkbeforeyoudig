6aa0891772ccba56c950c1b51b37f40e52c7ef06
Joyce Echessa <jokhessa@yahoo.com>
2012-06-14 00:19:45 +0300

    DBUG-7 continued working on the pdf generator

--- pdfgenerator.js
+++ pdfgenerator.js
@@ -9,15 +9,75 @@ var http = require('http');
 var url = require('url');
 var fs = require('fs');
 var PDFDocument = require('pdfkit-memory');
+var request = null;
 
 PDFGenerator = function() {
 };
 
 PDFGenerator.prototype.generatePDF = function(callback, emailAddress, enquiry) {
+	/*
 	console.log(enquiry);
-	doc = new PDFDocument();
-	doc.text("Check Before You Dig", {align : "center"});
-	mail(doc, emailAddress);
+		doc = new PDFDocument();
+		doc.text("Check Before You Dig", {align : "center"});
+		mail(doc, emailAddress);*/
+		
+		generate(emailAddress);
+
+}
+
+function generate(emailAddress) {
+	u = url.parse("http://maps.googleapis.com/maps/api/staticmap?sensor=false&center=-4.0434771,39.6682065&size=640x640&zoom=16");
+
+var client = http.createClient(80, u['host']);
+var request = client.request('GET', u['path'], {
+	"host" : u['host']
+});
+
+console.log('Request made ' + u['host'] + u['path']);
+
+doc = new PDFDocument();
+
+request.addListener('response', function(response) {
+	console.log("Response is " + response.statusCode);
+	if(response.statusCode == 200) {
+		response.setEncoding('binary');
+		var imageData = '';
+
+		var bufSize = 64 * 1024;
+		var bufPos = 0;
+		var buf = new Buffer(bufSize);
+
+		response.addListener('data', function(chunk) {
+			imageData += chunk;
+			var bufNextPos = bufPos + chunk.length;
+			if(bufNextPos == bufSize) {
+				buf.write(chunk, 'binary', bufPos);
+				res.write(buf);
+				bufPos = 0;
+			} else {
+				buf.write(chunk, 'binary', bufPos);
+				bufPos = bufNextPos;
+			}
+
+		});
+
+		response.addListener('end', function() {
+			//fs.writeFile('image.png', imageData, bufPos, 'binary', function(err) {
+				//if(err)
+					//throw err;
+				//console.log('File saved');
+				
+
+				if(bufPos != 0) {
+					buf = buf.slice(0, bufPos);
+				};
+				doc.imageFromBuffer(buf, 100, 100);
+				// doc.write('image.pdf');
+				mail(doc, emailAddress);
+			//});
+		});
+	}
+});
 }
 
 
@@ -29,6 +89,10 @@ var smtpTransport = nodemailer.createTransport("SMTP", {
 		pass : password
 	}
 });
+
+if(request) {
+	request.end();
+}
 
 // setup e-mail data with unicode symbols
 function mail(doc, emailAddress) {
